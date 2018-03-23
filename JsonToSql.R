865413962696e0107652088e02d5bc53c880ba22
Juan Pablo Moraga <jp@lemonpot.com>
2018-03-23 18:08:52 -0300

    Add files via upload

--- JsonToSql.R
+++ JsonToSql.R
@@ -10,37 +10,59 @@ json <- fromJSON(json[,2])
 
 json <- json$insights$insights_data$data
 
+campos <- c('account_id','account_name','account_currency','ad_id',
+            'ad_name','adset_id','adset_name','campaign_id','campaign_name','canvas_avg_view_percent','canvas_avg_view_time','clicks','cpc','cpm',
+            'cpp','ctr','date_start','date_stop','estimated_ad_recall_rate','estimated_ad_recallers','frequency','impressions','reach','social_clicks',
+            'social_impressions','social_reach','spend','total_action_value','total_actions','total_unique_actions','age','gender',
+            'video_10_sec_watched_actions','video_30_sec_watched_actions','video_p100_watched_actions','video_p25_watched_actions',
+            'video_p50_watched_actions','video_p75_watched_actions','video_p95_watched_actions')
 
-for(i in 1:length(json)) {
+campos1 <- c('actions')
+
+campos2 <- c('like','link_click','post_reaction','video_view','page_engagement','post_engagement')
+
+
+#for(i in 1:length(json)) {
+#for(i in 1:2) {
   
-  json1 <- as.data.frame(json[1])
+  json1 <- json[1]
   
-  for(j in 1:dim(json1)[2]) {
+  json1_1 <-json1[[1]][campos]
+  
+  json1_2 <- json1[[1]][campos1]
+  
+  json1_2 <- json1_2$actions
+  
+  json1_1 <- as.data.frame(json1_1)
+  
+  colnames(json1_1) <- campos
+  
+  json1_1 <- json1_1[2,]
+  
+  json1_2_df <- as.data.frame(matrix(nrow = 1,ncol = length(json1_2)))
+  
+  x <- NULL
+  x1 <- NULL
+  for(j in 1:length(json1_2)) {
+    c <- json1_2[[j]][1]
+    x <- cbind(x,c)
     
-    if(json1[1,j] != json1[2,j]) {
-      colnames(json1)[j] <- as.character(json1[,j][1])
-      
-    } else {
-      colnames(json1)[j] <- colnames(json1)[j]
-    }
+    c1 <- json1_2[[j]][2]
+    x1 <- cbind(x1,c1)
   }
   
-  json1 <- json1[2,]
+  colnames(json1_2_df) <- x
+  colnames(x1) <- x
+  json1_2_df <- rbind(x,x1)
   
-}
+  json1_2_df <- as.data.frame(json1_2_df)
+  
+  colnames(json1_2_df) <- x
+  
+  json1_2_df <- json1_2_df[2,]
+  
+  json_final <- cbind(json1_1,json1_2)
+  
+  x <- rbind(x,json_final)
+#}
 
-
-
-names <- names(json)
-vec <- names == 'account_id'
-filter <- which(vec == TRUE)
-
-
-
-m <- matrix(NA,length(filter),146)
-
-for(i in 2:length(filter)) {
-  m[i-1,] <- c(names[filter[i-1]:(filter[i]-1)],rep(0,146-length(names[filter[i-1]:(filter[i]-1)])))
-}
-
-#Seleccionar nombres
